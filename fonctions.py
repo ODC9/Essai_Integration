e34ed7fe9c9a1f5d0e2405cd124ea775b33d8bd5
Ourobè Donatien <cdonatien9@gmail.com>
2023-08-20 00:44:49 +0000

    Implementations des fonctions de notre calculatrice

--- fonctions.py
+++ fonctions.py
@@ -1,1 +1,44 @@
-essai
+def addition(a,b):
+    try:
+        nombre1 = float(a)
+        nombre2 = float(b)
+        return nombre1 + nombre2
+    except ValueError:
+        return "Erreur de saisie"
+
+    
+def soustraction(a,b):
+    try:
+        nombre1 = float(a)
+        nombre2 = float(b)
+        return nombre1 - nombre2
+    except ValueError:
+        return "Erreur de saisie"
+    
+
+def multiplication(a,b):
+    try:
+        nombre1 = float(a)
+        nombre2 = float(b)
+        return nombre1 * nombre2
+    except ValueError:
+        return "Erreur de saisie"
+
+def division(a,b):
+    try:
+        nombre1 = float(a)
+        nombre2 = float(b)
+        try:
+            return nombre1 / nombre2
+        except ZeroDivisionError:
+            return "La division par zero n'est pas possible"
+    except ValueError:
+        return "Erreur de saisie"
+
+def puissance(a, b):
+    try:
+        nombre1 = float(a)
+        nombre2 = float(b)
+        return nombre1**nombre2
+    except ValueError:
+        return "Erreur de saisie"
